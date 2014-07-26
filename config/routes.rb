2950e0fa9219990eb97e06b9b334ff76c1ec3dde
John Donner <johnbdonner@gmail.com>
2014-07-25 23:57:05 -0400

    Add a StaticPages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 DrSite::Application.routes.draw do
+  get "static_pages/home"
+  get "static_pages/about"
+  get "static_pages/donate"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
